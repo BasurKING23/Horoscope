b7b197379a4b00efa68979e727e1922281455bc3
abasurco0@gmail.com <abasurco0@gmail.com>
2025-05-12 13:36:40 +0200

    funcioanmiento de la 2da vista

---
+++ Horoscope/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  Horoscope
+//
+//  Created by Mañanas on 12/5/25.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailsimageview: UIImageView!
+    @IBOutlet weak var detailstitleview: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        
+    }
+    
+
+}
